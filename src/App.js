ad6d52edacae0f28e1f169c93399392a081dc1c9
JosiaAlmeida <josiaalmeida0001@gmail.com>
2021-08-10 10:40:50 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react'
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: 'Josia Almeida',
+      counter: 0
+    }
+    this.handleChangeClick = this.handleChangeClick.bind(this)
+    this.handleChangeName = this.handleChangeName.bind(this)
+  }
+  handleChangeName() {
+    const { name } = this.state
+    this.setState({ name: 'Josué' })
+  }
+  handleChangeClick() {
+    const { counter } = this.state
+    this.setState({ counter: counter + 1 })
+  }
+  render() {
+    const { name, counter } = this.state
+    return (
+      <div className="App">
+        <p onClick={this.handleChangeName}>
+          Welcome {name} - {counter}
+        </p>
+        <button onClick= {this.handleChangeClick}>
+          Click
+        </button>
+      </div>
+    );
+  }
+}
 
-function App() {
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +53,6 @@ function App() {
       </header>
     </div>
   );
-}
+}*/
 
 export default App;
