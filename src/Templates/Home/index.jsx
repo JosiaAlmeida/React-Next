23abbe3b634ca1d0aac1e722967482ba6bc4ec7c
JosiaAlmeida <josiaalmeida0001@gmail.com>
2021-08-12 23:07:19 +0100

    Migrando para React Hooks

--- src/Templates/Home/index.jsx
+++ src/Templates/Home/index.jsx
@@ -1,61 +1,50 @@
 import './style.css';
-import { Component } from 'react'
+import { Component,useState, useEffect } from 'react'
 import { loadPost } from '../../utils/load-post.js'
 import { Post } from '../../components/Post'
 import { Button } from '../../components/Button/index';
 import { Input } from '../../components/Input';
 
-class Home extends Component {
-  state = {
-    posts: [],
-    allPost: [],
-    page: 0,
-    postsPerPage: 2,
-    Blocked: false,
-    search: ''
-  }
-  loadPosts = async () => {
-    const { page, postsPerPage } = this.state
-    const postsAndPhotos = await loadPost()
-    this.setState({
-      posts: postsAndPhotos.slice(page, postsPerPage),
-      allPost: postsAndPhotos
-    })
-  }
-  async componentDidMount() {
-    await this.loadPosts()
-  }
+const Home = () => {
+  const [posts, setposts] = useState([])
+  const [allPost, setallPost] = useState([])
+  const [page, setpage] = useState(0)
+  const [postsPerPage, setpostsPerPage] = useState(2)
+  const [Blocked, setBlocked] = useState(false)
+  const [search, setsearch] = useState('')
 
-  loadMorePost = () => {
-    const { page, postsPerPage, allPost, posts } = this.state
+  const loadPosts = async () => {
+    const postsAndPhotos = await loadPost()
+    setposts(postsAndPhotos.slice(page, postsPerPage))
+    setallPost(postsAndPhotos)
+  }
+useEffect(() => {
+  loadPosts()
+}, [])
+  const loadMorePost = () => {
     const nextPage = page + postsPerPage
     posts.push(...allPost.slice(nextPage, nextPage + postsPerPage))
-    this.setState({
-      page: nextPage,
-      posts
-    })
-    if (posts.length == allPost.length) this.setState({ Blocked: true })
+    setposts(posts)
+    setpage(nextPage)
+    if (posts.length == allPost.length) setBlocked(true)
   }
-  handlesearchPost = async (e) => {
-    const { search, posts, allPost } = this.state
+  const handlesearchPost = async (e) => {
     const { value } = e.target
-    this.setState({ search: value })
-    if (!!value) this.setState({ posts: allPost.filter(post => post.title.toLowerCase().includes(search.toLowerCase())) })
-    else await this.loadPosts()
+    setsearch(value)
+    if (!!value) setposts(allPost.filter(post => post.title.toLowerCase().includes(search.toLowerCase())))
+    else await loadPosts()
   }
-  render() {
-    const { posts, Blocked, search } = this.state
-    return (
-      <div className="container">
-        <div className="searchContainer">
-          <Input search={search} handlesearchPost={this.handlesearchPost} />
-        </div>
-        <Post posts={posts} />
-        <div className="container-button" >
-          <Button disabled={Blocked} onClick={this.loadMorePost} />
-        </div>
+  return (
+    <div className="container">
+      <div className="searchContainer">
+        <Input search={search} handlesearchPost={handlesearchPost} />
       </div>
-    );
-  }
+      <Post posts={posts} />
+      <div className="container-button" >
+        <Button disabled={Blocked} onClick={loadMorePost} />
+      </div>
+    </div>
+  );
 }
+
 export default Home;
